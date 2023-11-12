531101c685d7dfa93802596654e4154724b027ae
ricokoro <77748744+ricokoro@users.noreply.github.com>
2023-11-12 20:59:38 +0800

    Add photo taking file and required libraries

--- Camerafile/Camerafile.ino
+++ Camerafile/Camerafile.ino
@@ -8,8 +8,8 @@
 // ===========================
 // Enter your WiFi credentials
 // ===========================
-const char* ssid = "Shawn";
-const char* password = "Shawnstevechang";
+const char* ssid = "Rich";
+const char* password = "12345678";
 
 void startCameraServer();
 void setupLedFlash(int pin);
